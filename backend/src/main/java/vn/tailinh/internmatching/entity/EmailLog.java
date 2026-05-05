3a5b40c0cdc229f098d200a6104142d5744de906
Linhdevdev <linhcoder2006@gmail.com>
2026-05-06 00:05:55 +0700

    update entity EmailLog

--- backend/src/main/java/vn/tailinh/internmatching/entity/EmailLog.java
+++ backend/src/main/java/vn/tailinh/internmatching/entity/EmailLog.java
@@ -9,9 +9,12 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
+import vn.tailinh.internmatching.util.constant.EmailStatus;
 
 @Entity
 @Table(name = "email_logs")
@@ -22,7 +25,7 @@ public class EmailLog {
 
 @Id 
 @GeneratedValue(strategy = GenerationType.IDENTITY)
-private long id;
+private Long id;
 
 @Column(name = "recipient_email")
 private String recipientEmail;
@@ -35,9 +38,13 @@ private String content;
 
 @Enumerated(EnumType.STRING)
 @Column(name = "status")
-private String status;
+private EmailStatus status;
 
 @Column(name = "sent_at")
 private Instant sentAt;
+
+@ManyToOne
+@JoinColumn(name = "user_id")
+private User user;
 
 }

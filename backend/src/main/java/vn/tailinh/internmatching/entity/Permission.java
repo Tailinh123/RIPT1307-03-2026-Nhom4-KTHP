1765e522918608956528af8ec5a9e0633c1da433
Linhdevdev <linhcoder2006@gmail.com>
2026-05-06 23:02:52 +0700

    update entity permissions

---
+++ backend/src/main/java/vn/tailinh/internmatching/entity/Permission.java
@@ -0,0 +1,39 @@
+package vn.tailinh.internmatching.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Table(name = "permissions")
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Permission extends AbstractAuditingEntity<Long>{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotBlank(message = "Name is required")
+    private String name;
+
+    @NotBlank(message = "API Path is required")
+    private String apiPath;
+
+    @NotBlank(message = "Method is required")
+    private String method;
+
+    @NotBlank(message = "Module is required")
+    private String module;
+
+    @ManyToMany(fetch = FetchType.LAZY, mappedBy = "permissions")
+    @JsonIgnore
+    private List<Role> roles;
+
+}

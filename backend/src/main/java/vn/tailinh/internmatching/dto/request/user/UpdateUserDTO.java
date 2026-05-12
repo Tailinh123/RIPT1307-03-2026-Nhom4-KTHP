107966615e9683c0475f0a1e15a1513d421c3803
Linhdevdev <linhcoder2006@gmail.com>
2026-05-12 17:06:16 +0700

    feat : usserDTO

---
+++ backend/src/main/java/vn/tailinh/internmatching/dto/request/user/UpdateUserDTO.java
@@ -0,0 +1,38 @@
+package vn.tailinh.internmatching.dto.request.user;
+
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Past;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import vn.tailinh.internmatching.entity.Company;
+import vn.tailinh.internmatching.entity.Role;
+import vn.tailinh.internmatching.util.constant.Gender;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateUserDTO {
+  private Long id;
+
+  @NotBlank(message = "Name cannot be blank")
+  private String name;
+
+  @Past(message = "Date of birth must be in the past")
+  @JsonFormat(pattern = "yyyy-MM-dd")
+  private LocalDate dateOfBirth;
+
+  private String address;
+
+  private Gender gender;
+
+  private Company company;
+
+  private Role role;
+}

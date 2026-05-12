70d6035513222bd38206bc1d43d9e96d02ff2556
Linhdevdev <linhcoder2006@gmail.com>
2026-05-12 17:11:36 +0700

    feat : Userservicee

---
+++ backend/src/main/java/vn/tailinh/internmatching/service/UserService.java
@@ -0,0 +1,136 @@
+package vn.tailinh.internmatching.service;
+
+import lombok.RequiredArgsConstructor;
+import vn.tailinh.internmatching.entity.User;
+import vn.tailinh.internmatching.dto.request.user.UpdateUserDTO;
+import vn.tailinh.internmatching.dto.response.ResultPaginationResponse;
+import vn.tailinh.internmatching.dto.response.user.CreatedUserResponse;
+import vn.tailinh.internmatching.dto.response.user.ResUserDTO;
+import vn.tailinh.internmatching.dto.response.user.UpdatedUserResponse;
+import vn.tailinh.internmatching.exception.IdInvalidException;
+import vn.tailinh.internmatching.repository.UserRepository;
+import vn.tailinh.internmatching.util.mapper.UserConvert;
+import vn.tailinh.internmatching.util.response.FormatResultPagaination;
+import vn.tailinh.internmatching.entity.Company;
+import vn.tailinh.internmatching.entity.Role;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    private final CompanyService companyService;
+    private final PasswordEncoder passwordEncoder;
+    private final RoleService roleService;
+
+    public CreatedUserResponse createUser(User user) throws Exception {
+        String email = user.getEmail();
+
+        if(user.getCompany() != null){
+            Company company = this.companyService.findCompanyById(user.getCompany().getId());
+            user.setCompany(company);
+        }
+
+        if(userRepository.existsByEmail(email)) {
+            throw new DataIntegrityViolationException("Email already exists");
+        }
+
+        if(user.getRole() != null){
+            Role role = this.roleService.fetchRoleById(user.getRole().getId());
+            user.setRole(role);
+        }
+
+        String hashPassword = this.passwordEncoder.encode(user.getPassword());
+        user.setPassword(hashPassword);
+
+        return UserConvert.convertToResCreatedUserRes(this.userRepository.save(user));
+    }
+
+
+    
+    public ResUserDTO fetchUserById(Long id) throws Exception {
+        if(userRepository.existsById(id)){
+            return UserConvert.convertToUserDTO(this.userRepository.findById(id).get());
+        }else{
+            throw new IdInvalidException("The specified User ID is invalid");
+        }
+    }
+
+
+
+    public void deleteUser(Long id) throws Exception {
+        if(this.userRepository.existsById(id)){
+            this.userRepository.deleteById(id);
+        }else{
+         throw new IdInvalidException("The specified User ID is invalid");
+        }
+    }
+
+
+
+    public User handleGetUserByUsername(String username) {
+        return this.userRepository.findByEmail(username);
+    }
+
+
+
+    public ResultPaginationResponse getAllUser(Pageable pageable, Specification<User> spec){
+        Page<User> userPage = this.userRepository.findAll(spec, pageable);
+
+        return FormatResultPagaination.createPaginateUserRes(userPage);
+    }
+
+
+
+    public UpdatedUserResponse updateUser(Long id, UpdateUserDTO requestUser) throws Exception {
+        Optional<User> userOptional = this.userRepository.findById(id);
+        if(userOptional.isPresent()){
+            User currentUser = userOptional.get();
+            currentUser.setName(requestUser.getName());
+            currentUser.setGender(requestUser.getGender());
+            currentUser.setAddress(requestUser.getAddress());
+            currentUser.setDateOfBirth(requestUser.getDateOfBirth());
+            if(requestUser.getCompany() != null){
+                Company company = this.companyService.findCompanyById(requestUser.getCompany().getId());
+                if(company == null){
+                    throw new IdInvalidException("Company Id is invalid");
+                }
+                currentUser.setCompany(company);
+            }
+            if(requestUser.getRole() != null){
+                Role role = this.roleService.fetchRoleById(requestUser.getRole().getId());
+                currentUser.setRole(role);
+            }
+
+            return UserConvert.convertToResUpdatedUserRes(this.userRepository.save(currentUser));
+        }
+        return null;
+    }
+
+
+
+    public void updateUserToken(String token, String email){
+        User user = this.handleGetUserByUsername(email);
+        if(user != null){
+            user.setRefreshToken(token);
+            this.userRepository.save(user);
+        }
+    }
+
+
+
+    public User getUserByRefreshTokenAndEmail(String token, String email){
+        return this.userRepository.findByRefreshTokenAndEmail(token, email);
+    }
+
+
+    
+}

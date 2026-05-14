3090a39f1a35ac81d3f3a9f0d0548191727c66a2
Linhdevdev <linhcoder2006@gmail.com>
2026-05-15 05:11:11 +0700

    feat: add JobcategoryService

--- backend/src/main/java/vn/tailinh/internmatching/service/JobCategoryService.java
+++ backend/src/main/java/vn/tailinh/internmatching/service/JobCategoryService.java
@@ -1,13 +1,55 @@
 package vn.tailinh.internmatching.service;
 
+import org.hibernate.query.Page;
+import org.springframework.boot.autoconfigure.batch.BatchProperties.Job;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
+import vn.tailinh.internmatching.dto.response.ResultPaginationResponse;
+import vn.tailinh.internmatching.entity.JobCategory;
+import vn.tailinh.internmatching.exception.IdInvalidException;
 import vn.tailinh.internmatching.repository.JobCategoryRepository;
+import vn.tailinh.internmatching.util.response.FormatRestResponse;
+import vn.tailinh.internmatching.util.response.FormatResultPagaination;
 
-@Service @RequiredArgsConstructor
+@Service
+@RequiredArgsConstructor
 public class JobCategoryService {
-    private final JobCategoryRepository jobCategoryRepository;
+  private final JobCategoryRepository jobCategoryRepository;
+
+  public JobCategoryRepository create(JobCategory jobcategory) throws Exception {
+    if (jobCategoryRepository.existsByName(jobcategory.getName())) {
+      throw new IdInvalidException("Category name already exists");
+    }
+    return jobCategoryRepository.save(jobcategory);
+  }
+
+  public JobCategory fetchById(Long Id) throws Exception {
+    return jobCategoryRepository.findById(Id).orElseThrow(() -> new IdInvalidException("JobCategory not found"));
+  }
+
+  public JobCategory update(JobCategory jobCategory) throws Exception {
+    JobCategory current = this.fetchById(jobCategory.getId());
+    current.setName(jobCategory.getName());
+    current.setDescription(jobCategory.getDescription());
+
+    return jobCategoryRepository.save(current);
+  }
+
+  public void delete(Long id) throws Exception {
+    this.fetchById(id);     //  kiểm tra exists
+    jobCategoryRepository.deleteById(id);
+  }
+
+
+  
+  public ResultPaginationResponse fetchAll(Specification<JobCategory> specification , Pageable pageable ) {
+    Page<JobCategory> page = jobCategoryRepository.findAll(specification , pageable);
+    return FormatResultPagaination.createPaginationResponse(page); 
+  }
+  
 
 
 }

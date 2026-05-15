c94b3960a335a0d117a81f35b31170630e7c39a2
Linhdevdev <linhcoder2006@gmail.com>
2026-05-15 22:53:30 +0700

    refactor: clean logic

--- backend/src/main/java/vn/tailinh/internmatching/util/mapper/ApplicationMapper.java
+++ backend/src/main/java/vn/tailinh/internmatching/util/mapper/ApplicationMapper.java
@@ -1,5 +1,6 @@
 package vn.tailinh.internmatching.util.mapper;
 
+import vn.tailinh.internmatching.dto.request.application.CreateApplicationDTO;
 import vn.tailinh.internmatching.dto.response.application.CreateApplicationResponse;
 import vn.tailinh.internmatching.dto.response.application.FetchApplicationResponse;
 import vn.tailinh.internmatching.entity.Application;
@@ -7,6 +8,35 @@ import vn.tailinh.internmatching.entity.Application;
 public class ApplicationMapper {
   public static CreateApplicationResponse toCreateApplicationResponse(Application application) {
     CreateApplicationResponse res = new CreateApplicationResponse();
+
+    res.setId(application.getId());
+    res.setStatus(application.getStatus());
+    res.setNote(application.getNote());
+    res.setCreatedAt(application.getCreatedAt());
+    res.setCreatedBy(application.getCreatedBy());
+
+    // resume
+    if (application.getResume() != null) {
+      CreateApplicationResponse.ResumeDTO resume = new CreateApplicationResponse.ResumeDTO();
+      resume.setId(application.getResume().getId());
+      resume.setUrl(application.getResume().getUrl());
+      res.setResume(resume);
+    }
+
+    // Job
+
+    if (application.getJob() != null) {
+      CreateApplicationResponse.JobDTO job = new CreateApplicationResponse.JobDTO();
+      job.setId(application.getJob().getId());
+      job.setName(application.getJob().getName());
+
+      res.setJob(job);
+    }
+    return res;
+  }
+
+  public static FetchApplicationResponse toFetchApplicationResponse(Application application) {
+    FetchApplicationResponse res = new FetchApplicationResponse();
     res.setId(application.getId());
     res.setStatus(application.getStatus());
     res.setNote(application.getNote());
@@ -15,53 +45,15 @@ public class ApplicationMapper {
     res.setUpdatedAt(application.getUpdatedAt());
     res.setUpdatedBy(application.getUpdatedBy());
 
-
-
-    // resume
-    if (application.getResume() != null) {
-      CreateApplicationResponse.ResumeDTO resume = new CreateApplicationResponse.ResumeDTO();
-      resume.setId(application.getResume().getId());
-      resume.setTitle(application.getResume().getTitle());
-      resume.setUrl(application.getResume().getUrl());
-      res.setResume(resume);
-    }
-
-
-    // Job 
-
-    if ( application.getJob() != null ) {
-      CreateApplicationResponse.JobDTO job = new CreateApplicationResponse.JobDTO();
+    // job
+    if (application.getJob() != null) {
+      FetchApplicationResponse.JobDTO job = new FetchApplicationResponse.JobDTO();
       job.setId(application.getJob().getId());
-      job.setName(application.getJob().getName());
 
       res.setJob(job);
     }
-      return res;
-  }
 
-
-     public static FetchApplicationResponse toFetchApplicationResponse(Application application) {
-        FetchApplicationResponse res = new FetchApplicationResponse();
-        res.setId(application.getId());
-        res.setStatus(application.getStatus());
-        res.setNote(application.getNote());
-        res.setCreatedAt(application.getCreatedAt());
-        res.setCreatedBy(application.getCreatedBy());
-        res.setUpdatedAt(application.getUpdatedAt());
-        res.setUpdatedBy(application.getUpdatedBy());
-
-        
-
-        // job 
-        if( application.getJob() != null) {
-          FetchApplicationResponse.JobDTO job = new FetchApplicationResponse.JobDTO();
-          job.setId(application.getJob().getId());
-
-          res.setJob(job);
-        }
-        
-
-            // resume
+    // resume
     if (application.getResume() != null) {
       FetchApplicationResponse.ResumeDTO resume = new FetchApplicationResponse.ResumeDTO();
       resume.setId(application.getResume().getId());
@@ -69,7 +61,7 @@ public class ApplicationMapper {
       resume.setUrl(application.getResume().getUrl());
       res.setResume(resume);
     }
-      return res;
-      }
-      
+    return res;
+  }
+
 }

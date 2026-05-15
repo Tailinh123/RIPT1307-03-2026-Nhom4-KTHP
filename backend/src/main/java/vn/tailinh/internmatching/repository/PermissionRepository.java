ef42eb28c60243218c42f92d1877312991ad7ca0
Linhdevdev <linhcoder2006@gmail.com>
2026-05-15 23:14:31 +0700

    feat: add permissionRepo

---
+++ backend/src/main/java/vn/tailinh/internmatching/repository/PermissionRepository.java
@@ -0,0 +1,20 @@
+package vn.tailinh.internmatching.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+import vn.tailinh.internmatching.entity.Permission;
+
+import java.util.List;
+
+@Repository
+public interface PermissionRepository extends
+        JpaRepository<Permission, Long>, JpaSpecificationExecutor<Permission> {
+
+    boolean existsByModuleAndApiPathAndMethod(String module, String apiPath, String method);
+
+    List<Permission> findByIdIn(List<Long> id);
+
+    boolean existsByName(String name);
+}

42ad6bb7712b3c579c9e32cffaae3c873bec304a
Linhdevdev <linhcoder2006@gmail.com>
2026-05-17 00:21:22 +0700

    fix: fix logic Application

--- backend/src/main/java/vn/tailinh/internmatching/service/ApplicationService.java
+++ backend/src/main/java/vn/tailinh/internmatching/service/ApplicationService.java
@@ -2,21 +2,17 @@ package vn.tailinh.internmatching.service;
 
 import java.util.Optional;
 
-import org.springframework.boot.autoconfigure.batch.BatchProperties.Job;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
-import vn.tailinh.internmatching.dto.request.application.CreateApplicationDTO;
 import vn.tailinh.internmatching.dto.response.application.CreateApplicationResponse;
 import vn.tailinh.internmatching.entity.Application;
+import vn.tailinh.internmatching.entity.Job;
 import vn.tailinh.internmatching.entity.Resume;
 import vn.tailinh.internmatching.exception.IdInvalidException;
 import vn.tailinh.internmatching.repository.ApplicationRepository;
-import vn.tailinh.internmatching.repository.JobCategoryRepository;
 import vn.tailinh.internmatching.repository.JobRepository;
 import vn.tailinh.internmatching.repository.ResumeRepository;
-import vn.tailinh.internmatching.repository.UserRepository;
-import vn.tailinh.internmatching.util.constant.ApplicationStatus;
 import vn.tailinh.internmatching.util.mapper.ApplicationMapper;
 
 
@@ -29,30 +25,33 @@ public class ApplicationService {
     private final ResumeRepository resumeRepository;
 
 
-    public CreateApplicationResponse create(CreateApplicationDTO dto) throws Exception {
+    public CreateApplicationResponse create(Application application) throws Exception {
 
       // check job tồn tại 
-      Optional<Job> jobOptional = this.jobRepository.findById(dto.getJob().getId());
+      Optional<Job> jobOptional = this.jobRepository.findById(application.getJob().getId());
       if (jobOptional.isEmpty()) {
         throw new IdInvalidException("Job not found");
       }
 
-      // check reusume tồn tại 
-      Optional<Resume> resumeOptional = this.resumeRepository.findById(dto.getResume().getId());
+      // check resume tồn tại 
+      Optional<Resume> resumeOptional = this.resumeRepository.findById(application.getResume().getId());
       if(resumeOptional.isEmpty()) {
         throw new IdInvalidException("Resume not found");
       }
 
-       Application app = new Application();
-        app.setJob(jobOptional.get());
-        app.setResume(resumeOptional.get());
-        app.setStatus(ApplicationStatus.PENDING);
-        app.setNote(dto.getNote());
+      application = this.applicationRepository.save(application);
 
-        return ApplicationMapper.toCreateApplicationResponse(
-            this.applicationRepository.save(app));
+
+
+        return ApplicationMapper.toCreateApplicationResponse(application);
     
     }
+
+
+    // public UpdateApplicationResponse update(UpdateApplicationDTO dto ) throws Exception {
+
+      
+    // }
 
 
 }

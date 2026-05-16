64adda7136a699f2d858da848c84605c28d283c2
Linhdevdev <linhcoder2006@gmail.com>
2026-05-17 00:12:49 +0700

    fix: remove import library

--- backend/src/main/java/vn/tailinh/internmatching/service/JobCategoryService.java
+++ backend/src/main/java/vn/tailinh/internmatching/service/JobCategoryService.java
@@ -1,11 +1,16 @@
 package vn.tailinh.internmatching.service;
 
+import org.hibernate.query.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
+import vn.tailinh.internmatching.dto.response.ResultPaginationResponse;
 import vn.tailinh.internmatching.entity.JobCategory;
 import vn.tailinh.internmatching.exception.IdInvalidException;
 import vn.tailinh.internmatching.repository.JobCategoryRepository;
+import vn.tailinh.internmatching.util.response.FormatResultPagaination;
 
 
 @Service
@@ -39,10 +44,7 @@ public class JobCategoryService {
 
 
   
-  // public ResultPaginationResponse fetchAll(Specification<JobCategory> specification , Pageable pageable ) {
-  //   Page<JobCategory> page = jobCategoryRepository.findAll(specification , pageable);
-  //   return FormatResultPagaination.createPaginationResponse(page); 
-  // }
+
   
 
 

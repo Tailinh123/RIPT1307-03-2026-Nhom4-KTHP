fa17550181d8c50c2fbd72be4abd5e5990ecdcf4
Linhdevdev <linhcoder2006@gmail.com>
2026-05-17 00:26:30 +0700

    fix: remove inner class UserLogin and remove import

--- backend/src/main/java/vn/tailinh/internmatching/dto/response/auth/LoginResponse.java
+++ backend/src/main/java/vn/tailinh/internmatching/dto/response/auth/LoginResponse.java
@@ -6,44 +6,42 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import vn.tailinh.internmatching.entity.Role;
 
 @Getter
 @Setter
 public class LoginResponse {
-    @JsonProperty("access_token")
-    private String accessToken;
-    private UserLogin user;
+  @JsonProperty("access_token")
+  private String accessToken;
+  private UserLogin user;
 
-    @Getter
-    @Setter
-    @NoArgsConstructor
-    @AllArgsConstructor
-    public static class UserLogin{
-        private Long id;
-        private String email;
-        private String name;
-        private Role role;
-    }
+  @Getter
+  @Setter
+  @NoArgsConstructor
+  @AllArgsConstructor
+  public static class UserLogin {
+    private Long id;
+    private String email;
+    private String name;
+    private RoleDTO role;
+  }
 
-    @Getter
-    @Setter
-    @NoArgsConstructor
-    @AllArgsConstructor
-    public static class UserGetAccout {
-        private UserLogin user;
-    }
+  @Getter
+  @Setter
+  public static class RoleDTO {
+    private Long id;
+    private String name;
+  }
 
-    @Getter
-    @Setter
-    @NoArgsConstructor
-    @AllArgsConstructor
-    public static class UserInsideToken{
-        private Long id;
-        private String email;
-        private String name;
+  @Getter
+  @Setter
+  @NoArgsConstructor
+  @AllArgsConstructor
+  public static class UserInsideToken {
+    private Long id;
+    private String email;
+    private String name;
 
-        // List permissions làm menu ẩn/hiện
-        private java.util.List<String> permissions;
-    }
+    // List permissions làm menu ẩn/hiện
+    private java.util.List<String> permissions;
+  }
 }

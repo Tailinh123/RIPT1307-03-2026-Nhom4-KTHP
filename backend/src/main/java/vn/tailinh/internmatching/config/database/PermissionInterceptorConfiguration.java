ba77988a7ddca60b16dc165f61740b5f9b99d20b
Linhdevdev <linhcoder2006@gmail.com>
2026-05-20 01:44:01 +0700

    fix: add and remove api in whitList

--- backend/src/main/java/vn/tailinh/internmatching/config/database/PermissionInterceptorConfiguration.java
+++ backend/src/main/java/vn/tailinh/internmatching/config/database/PermissionInterceptorConfiguration.java
@@ -10,17 +10,23 @@ import vn.tailinh.internmatching.config.security.PermissionInterceptor;
 @Configuration
 public class PermissionInterceptorConfiguration implements WebMvcConfigurer {
 
-    @Bean
-    PermissionInterceptor getPermissionInterceptor() {
-        return new PermissionInterceptor();
-    }
-    @Override
+  @Bean
+  PermissionInterceptor getPermissionInterceptor() {
+    return new PermissionInterceptor();
+  }
+
+  @Override
     public void addInterceptors(InterceptorRegistry registry) {
         String[] whiteList = {
-                "/", "/api/v1/auth/**", "/storage/**",
-                "/api/v1/companies", "/api/v1/jobs", "/api/v1/skills", "/api/v1/files",
+                "/", "/api/v1/auth/**",
+                 "/storage/**",
                 "/api/v1/resumes/**",
-                "/api/v1/subcribers/**"
+                "/api/v1/subscribers/**",
+                "/api/v1/users/change-password",
+                "/api/v1/companies/**",      
+                "/api/v1/jobs/**",       
+                "/api/v1/skills/**",         
+                "/api/v1/files/**" 
         };
         registry.addInterceptor(getPermissionInterceptor())
                 .excludePathPatterns(whiteList);

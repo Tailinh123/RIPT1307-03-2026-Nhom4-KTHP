d779b6275aa2c0e52e4290e42dd20b3829eba18d
Linhdevdev <linhcoder2006@gmail.com>
2026-05-20 01:39:40 +0700

    refactor: remove import

--- backend/src/main/java/vn/tailinh/internmatching/service/JobCategoryService.java
+++ backend/src/main/java/vn/tailinh/internmatching/service/JobCategoryService.java
@@ -1,16 +1,14 @@
 package vn.tailinh.internmatching.service;
 
-import org.hibernate.query.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.domain.Specification;
+
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
-import vn.tailinh.internmatching.dto.response.ResultPaginationResponse;
+
 import vn.tailinh.internmatching.entity.JobCategory;
 import vn.tailinh.internmatching.exception.IdInvalidException;
 import vn.tailinh.internmatching.repository.JobCategoryRepository;
-import vn.tailinh.internmatching.util.response.FormatResultPagaination;
+
 
 
 @Service

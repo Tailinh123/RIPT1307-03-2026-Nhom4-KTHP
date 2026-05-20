66c43124efba9a00aaefec2efe148df9a808b157
Linhdevdev <linhcoder2006@gmail.com>
2026-05-20 23:52:34 +0700

    feat: add logic JobcategoryController

--- backend/src/main/java/vn/tailinh/internmatching/controller/JobCategoryController.java
+++ backend/src/main/java/vn/tailinh/internmatching/controller/JobCategoryController.java
@@ -1,8 +1,64 @@
 package vn.tailinh.internmatching.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.service.annotation.PatchExchange;
 
-@Controller
+import com.turkraft.springfilter.boot.Filter;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import vn.tailinh.internmatching.entity.JobCategory;
+import vn.tailinh.internmatching.repository.JobCategoryRepository;
+import vn.tailinh.internmatching.service.JobCategoryService;
+import vn.tailinh.internmatching.util.annotation.ApiMessage;
+
+import java.lang.invoke.ClassSpecializer.SpeciesData;
+
+import org.hibernate.query.Page;
+import org.springdoc.core.converters.models.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+@RequestMapping(path = "${apiPrefix}/job-categories")
+@RestController
+@RequiredArgsConstructor
 public class JobCategoryController {
-  
+
+  private final JobCategoryService jobCategoryService;
+
+  @PostMapping("")
+  @ApiMessage("Create a JobCategory")
+  public ResponseEntity<JobCategory> create(@Valid @RequestBody JobCategory jobCategory) throws Exception {
+    return ResponseEntity.status(HttpStatus.CREATED).body(this.jobCategoryService.create(jobCategory));
+  }
+
+  @PutMapping("")
+  @ApiMessage("Update a Jobcategory")
+  public ResponseEntity<JobCategory> update(@Valid @RequestBody JobCategory jobCategory) throws Exception {
+    return ResponseEntity.ok().body(this.jobCategoryService.update(jobCategory));
+  }
+
+  @GetMapping("")
+  @ApiMessage("Fetch all JobCategory ")
+  public ResponseEntity<JobCategory> getAll(@Filter Specification<JobCategory> specification , Pageable pageable) {
+    return ResponseEntity.status(HttpStatus.OK).body(this.jobCategoryService.fetchAlljobCategory(specification , pageable));
+  }
+
+  @DeleteMapping("")
+  @ApiMessage("Deleta a JobCategory")
+  public ResponseEntity<Void> delete(@PathVariable("id") Long id) throws Exception{
+    this.jobCategoryService.delete(id);
+    return ResponseEntity.ok().body(null);
+  }
+
 }

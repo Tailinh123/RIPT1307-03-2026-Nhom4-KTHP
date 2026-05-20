1902bf93f0f8002c60207a00bc166422b323d89e
Linhdevdev <linhcoder2006@gmail.com>
2026-05-20 22:37:34 +0700

    feat: add extends JpaSpecificationExecutor

--- backend/src/main/java/vn/tailinh/internmatching/repository/JobCategoryRepository.java
+++ backend/src/main/java/vn/tailinh/internmatching/repository/JobCategoryRepository.java
@@ -2,10 +2,11 @@ package vn.tailinh.internmatching.repository;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import vn.tailinh.internmatching.entity.JobCategory;
 
-public interface JobCategoryRepository extends JpaRepository<JobCategory , Long > {
+public interface JobCategoryRepository extends JpaRepository<JobCategory , Long > , JpaSpecificationExecutor<JobCategory> {
   
 boolean existsByName(String name);
 }

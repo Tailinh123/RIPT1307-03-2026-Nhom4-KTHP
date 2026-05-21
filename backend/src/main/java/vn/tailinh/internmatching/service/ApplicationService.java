7d4eae0510c322097cfebc1ac087bb42dd0e96f7
Linhdevdev <linhcoder2006@gmail.com>
2026-05-21 16:46:21 +0700

    fix: bug in logic return

--- backend/src/main/java/vn/tailinh/internmatching/service/ApplicationService.java
+++ backend/src/main/java/vn/tailinh/internmatching/service/ApplicationService.java
@@ -1,6 +1,5 @@
 package vn.tailinh.internmatching.service;
 
-import java.security.PublicKey;
 import java.util.Optional;
 
 import org.springframework.data.domain.Page;
@@ -10,7 +9,6 @@ import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
-import vn.tailinh.internmatching.controller.ResumeController;
 import vn.tailinh.internmatching.dto.request.application.UpdateApplicationDTO;
 import vn.tailinh.internmatching.dto.response.ResultPaginationResponse;
 import vn.tailinh.internmatching.dto.response.application.CreateApplicationResponse;
@@ -26,15 +24,12 @@ import vn.tailinh.internmatching.repository.ResumeRepository;
 import vn.tailinh.internmatching.repository.UserRepository;
 import vn.tailinh.internmatching.security.SecurityUtils;
 import vn.tailinh.internmatching.util.mapper.ApplicationMapper;
-import vn.tailinh.internmatching.util.response.FormatRestResponse;
 import vn.tailinh.internmatching.util.response.FormatResultPagination;
 
 @Service
 @RequiredArgsConstructor
 public class ApplicationService {
 
-  private final ResumeController resumeController;
-  private final FormatRestResponse formatRestResponse;
   private final ApplicationRepository applicationRepository;
   private final JobRepository jobRepository;
   private final ResumeRepository resumeRepository;
@@ -42,13 +37,13 @@ public class ApplicationService {
 
   public CreateApplicationResponse create(Application application) throws Exception {
 
-    // check job tồn tại
+    // check job 
     Optional<Job> jobOptional = this.jobRepository.findById(application.getJob().getId());
     if (jobOptional.isEmpty()) {
       throw new IdInvalidException("Job not found");
     }
 
-    // check resume tồn tại
+    // check resume 
     Optional<Resume> resumeOptional = this.resumeRepository.findById(application.getResume().getId());
     if (resumeOptional.isEmpty()) {
       throw new IdInvalidException("Resume not found");
@@ -67,8 +62,8 @@ public class ApplicationService {
     Application currentApp = appOptional.get();
     currentApp.setStatus(dto.getStatus());
     currentApp.setNote(dto.getNote());
-    this.applicationRepository.save(currentApp);
-    return dto;
+    currentApp = this.applicationRepository.save(currentApp);
+    return ApplicationMapper.toUpdateApplicationResponse(currentApp);
 
   }
 
@@ -89,11 +84,11 @@ public class ApplicationService {
   public ResultPaginationResponse fetchByUser(Pageable pageable) {
       String email = SecurityUtils.getCurrentUserLogin().orElse("");
       User user = this.userRepository.findByEmail(email);
-      if(user = null) {
+      if(user == null) {
         return FormatResultPagination.createPaginationResponse(Page.empty());
       }
 
-      Page<Application> page = this.applicationRepository.findByResumeUserId(user.getId() , pageable);\
+      Page<Application> page = this.applicationRepository.findByResumeUserId(user.getId() , pageable);
       return FormatResultPagination.createPaginationResponse(page);
     }
 }

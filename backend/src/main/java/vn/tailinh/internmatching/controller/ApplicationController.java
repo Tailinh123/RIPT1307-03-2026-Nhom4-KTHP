848e6231305cacee7679a68c08b049a48b2af832
Linhdevdev <linhcoder2006@gmail.com>
2026-05-21 17:50:02 +0700

    fix: bug in crud

--- backend/src/main/java/vn/tailinh/internmatching/controller/ApplicationController.java
+++ backend/src/main/java/vn/tailinh/internmatching/controller/ApplicationController.java
@@ -6,18 +6,16 @@ import com.turkraft.springfilter.boot.Filter;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import vn.tailinh.internmatching.dto.request.application.CreateApplicationDTO;
+
 import vn.tailinh.internmatching.dto.request.application.UpdateApplicationDTO;
 import vn.tailinh.internmatching.dto.response.ResultPaginationResponse;
 import vn.tailinh.internmatching.dto.response.application.CreateApplicationResponse;
+import vn.tailinh.internmatching.dto.response.application.UpdateApplicationResponse;
 import vn.tailinh.internmatching.entity.Application;
-import vn.tailinh.internmatching.repository.ApplicationRepository;
 import vn.tailinh.internmatching.service.ApplicationService;
 import vn.tailinh.internmatching.util.annotation.ApiMessage;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import javax.naming.spi.DirStateFactory.Result;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
@@ -30,8 +28,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-
-
 @RequestMapping(path = "${apiPrefix}/applications")
 
 @RestController
@@ -39,40 +35,38 @@ import org.springframework.web.bind.annotation.PathVariable;
 public class ApplicationController {
   private final ApplicationService applicationService;
 
-
   @PostMapping("")
-  @ApiMessage("Create an application")
+  @ApiMessage("Create an application status")
   public ResponseEntity<CreateApplicationResponse> create(
-    @Valid @RequestBody Application application ) throws Exception {
-      return ResponseEntity.status(HttpStatus.CREATED).body(this.applicationService.create(application));
-    }
-
-    @PutMapping("")
-    @ApiMessage("Update an application startus")
-    public ResponseEntity<UpdateApplicationDTO> update(@Valid @RequestBody UpdateApplicationDTO dto )  throws Exception {
-      return ResponseEntity.ok(applicationService.updateStatus(dto));
-    }       
-    
-    @GetMapping("")
-    @ApiMessage("fetch all skills")
-    public ResponseEntity<ResultPaginationResponse> fetchAll(
-      @Filter Specification<Application> specification , Pageable pageable ) {
-        return ResponseEntity.ok().body(this.applicationService.fetchAllApplication(specification , pageable));   
-      }
-
-        @DeleteMapping("/{id}")
-    @ApiMessage("Delete a application")
-    public ResponseEntity<Void> delete(@PathVariable("id") Long id) throws Exception{
-        this.applicationService.deleteApplication(id);
-        return ResponseEntity.ok().body(null);
-    }
-
-    @PostMapping("/by-user")
-    public ResponseEntity<ResultPaginationResponse> fetchByUser(Pageable pageable) {
-        return ResponseEntity.ok(ApplicationService.fetchByUser(pageable));
-    }
-    
-  
-  
+      @Valid @RequestBody Application application) throws Exception {
+    return ResponseEntity.status(HttpStatus.CREATED).body(this.applicationService.create(application));
   }
-  
+
+
+  @PutMapping("")
+  @ApiMessage("Update an application startus")
+  public ResponseEntity<UpdateApplicationResponse> update(@Valid @RequestBody UpdateApplicationDTO dto) throws Exception {
+    return ResponseEntity.ok(applicationService.update(dto));
+  }
+
+
+  @GetMapping("")
+  @ApiMessage("fetch all applications")
+  public ResponseEntity<ResultPaginationResponse> fetchAll(
+      @Filter Specification<Application> specification, Pageable pageable) {
+    return ResponseEntity.ok().body(this.applicationService.fetchAllApplication(specification, pageable));
+  }
+
+  @DeleteMapping("/{id}")
+  @ApiMessage("Delete a application")
+  public ResponseEntity<Void> delete(@PathVariable("id") Long id) throws Exception {
+    this.applicationService.deleteApplication(id);
+    return ResponseEntity.ok().body(null);
+  }
+
+  @PostMapping("/by-user")
+  public ResponseEntity<ResultPaginationResponse> fetchByUser(Pageable pageable) {
+    return ResponseEntity.ok(applicationService.fetchByUser(pageable));
+  }
+
+}

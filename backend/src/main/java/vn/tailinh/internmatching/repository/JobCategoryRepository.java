9617ccce95ff55cd7683a01874b21a4395462214
Linhdevdev <linhcoder2006@gmail.com>
2026-05-21 20:09:48 +0700

    feat: add JobcategoryRepository

--- backend/src/main/java/vn/tailinh/internmatching/repository/JobCategoryRepository.java
+++ backend/src/main/java/vn/tailinh/internmatching/repository/JobCategoryRepository.java
@@ -1,6 +1,5 @@
 package vn.tailinh.internmatching.repository;
 
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 

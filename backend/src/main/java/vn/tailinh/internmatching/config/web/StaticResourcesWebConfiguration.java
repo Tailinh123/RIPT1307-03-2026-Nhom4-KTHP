a2c27a5dbf238b517dce7a8ebfd6c834a848786a
Linhdevdev <linhcoder2006@gmail.com>
2026-05-22 00:33:52 +0700

    feat: add StaticResuorceWebConfiguratio

---
+++ backend/src/main/java/vn/tailinh/internmatching/config/web/StaticResourcesWebConfiguration.java
@@ -0,0 +1,18 @@
+package vn.tailinh.internmatching.config.web;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class StaticResourcesWebConfiguration
+        implements WebMvcConfigurer {
+    @Value("${tailinh.upload-file.base-uri}")
+    private String baseURI;
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/storage/**")
+                .addResourceLocations(baseURI);
+    }
+}

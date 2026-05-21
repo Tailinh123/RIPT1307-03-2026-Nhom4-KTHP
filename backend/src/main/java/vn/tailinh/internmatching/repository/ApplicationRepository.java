72b68b9042b62cef16f642d541f9b0666baebe1d
Linhdevdev <linhcoder2006@gmail.com>
2026-05-21 16:45:10 +0700

    feat: add query logic

--- backend/src/main/java/vn/tailinh/internmatching/repository/ApplicationRepository.java
+++ backend/src/main/java/vn/tailinh/internmatching/repository/ApplicationRepository.java
@@ -1,12 +1,18 @@
 package vn.tailinh.internmatching.repository;
 
 
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import vn.tailinh.internmatching.entity.Application;
 
 
-public interface ApplicationRepository extends JpaRepository< Application , Long > ,JpaSpecificationExecutor<Application> {
+public interface ApplicationRepository extends
+ JpaRepository< Application , Long > 
+ ,JpaSpecificationExecutor<Application> {
+
+  Page<Application> findByResumeUserId(Long userId, Pageable pageable);
+  
 }

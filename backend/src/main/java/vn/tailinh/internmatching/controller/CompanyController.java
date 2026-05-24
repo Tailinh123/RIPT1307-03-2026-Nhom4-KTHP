16797ee3bc53753822e40a10d6a7a5926c2e9c02
Linhdevdev <linhcoder2006@gmail.com>
2026-05-24 15:48:32 +0700

    fix: add Exception

--- backend/src/main/java/vn/tailinh/internmatching/controller/CompanyController.java
+++ backend/src/main/java/vn/tailinh/internmatching/controller/CompanyController.java
@@ -12,7 +12,14 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @RequestMapping(path = "${apiPrefix}/companies")
@@ -23,18 +30,16 @@ public class CompanyController {
 
     @PostMapping("")
     @ApiMessage("Create a company")
-    public ResponseEntity<Company> createCompany(@Valid @RequestBody Company company){
+    public ResponseEntity<Company> createCompany(@Valid @RequestBody Company company) throws Exception {
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(this.companyService.createCompany(company));
     }
 
-    
     @GetMapping("")
     @ApiMessage("Fetch all company data")
     public ResponseEntity<ResultPaginationResponse> getAllCompany(
             @Filter Specification<Company> spec,
-            Pageable pageable
-    ){
+            Pageable pageable) {
         return ResponseEntity.status(HttpStatus.OK)
                 .body(this.companyService.getAllCompany(pageable, spec));
     }
@@ -42,23 +47,21 @@ public class CompanyController {
     @PutMapping("")
     @ApiMessage("Update a company")
     public ResponseEntity<Company> updateCompany(
-            @Valid @RequestBody Company company
-    ){
-        return ResponseEntity.ok(this.companyService.updateCompany(company));
+            @Valid @RequestBody Company company) throws Exception {
+        return ResponseEntity.status(HttpStatus.OK).body(this.companyService.updateCompany(company));
     }
 
     @DeleteMapping("/{id}")
     @ApiMessage("Delete a company")
     public ResponseEntity<Void> deleteCompany(
-            @PathVariable("id") Long id
-    ){
+            @PathVariable("id") Long id) throws Exception {
         this.companyService.deleteCompany(id);
         return ResponseEntity.ok(null);
     }
 
     @GetMapping("/{id}")
     @ApiMessage("Get company by id")
-    public ResponseEntity<Company> getCompany(@PathVariable("id") Long id) throws Exception{
+    public ResponseEntity<Company> getCompany(@PathVariable("id") Long id) throws Exception {
         return ResponseEntity.ok().body(this.companyService.findCompanyById(id));
     }
 }

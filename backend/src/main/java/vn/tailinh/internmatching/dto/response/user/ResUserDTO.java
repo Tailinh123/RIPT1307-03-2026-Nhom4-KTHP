cd855c94a9617d628fedf8602dc711e34feb3f70
Linhdevdev <linhcoder2006@gmail.com>
2026-05-28 15:01:03 +0700

    feat: add field response

--- backend/src/main/java/vn/tailinh/internmatching/dto/response/user/ResUserDTO.java
+++ backend/src/main/java/vn/tailinh/internmatching/dto/response/user/ResUserDTO.java
@@ -1,11 +1,12 @@
 package vn.tailinh.internmatching.dto.response.user;
 
 import java.time.LocalDate;
-
+import java.util.List;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import vn.tailinh.internmatching.entity.Skill;
 import vn.tailinh.internmatching.util.constant.Gender;
 @Setter
 @Getter
@@ -18,19 +19,25 @@ public class ResUserDTO {
 
   private String email;
 
-
   private LocalDate dateOfBirth;
 
   private Gender gender;
 
   private String address;
 
+  private String phone;
+
+  private String avatarUrl;
 
   private boolean isSubscribed;
 
-
   private CompanyDTO company;
+
   private RoleDTO role;
+
+  private List<Skill> skills;
+
+  
   @Setter
   @Getter
   @NoArgsConstructor

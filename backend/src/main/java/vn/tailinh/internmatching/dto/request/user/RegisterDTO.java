96ae9d2b7230689a79a2bec72058f056567a179d
Linhdevdev <linhcoder2006@gmail.com>
2026-05-28 23:08:44 +0700

    feat: add RegisterDTO

---
+++ backend/src/main/java/vn/tailinh/internmatching/dto/request/user/RegisterDTO.java
@@ -0,0 +1,25 @@
+package vn.tailinh.internmatching.dto.request.user;
+
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterDTO {
+  @NotBlank(message = "Name is required")
+  private String name;
+
+  @NotBlank(message = "Please provide a valid email address")
+  private String email;
+
+  @NotBlank(message = "Password cannot be blank")
+  private String password;
+
+}

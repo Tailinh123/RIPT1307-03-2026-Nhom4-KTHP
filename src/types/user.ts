63bf770963072ce76c42aebe165bc0935f402b75
lutrongdai <lutrongd@gmail.com>
2026-05-30 07:19:30 +0700

    add field

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,19 +1,24 @@
 export interface UserProfile {
   id: number;
   fullName: string;
+  name?: string;
   email: string;
   phone?: string;
   gender?: 'MALE' | 'FEMALE' | 'OTHER';
-  dob?: string;      
+  dob?: string;
+  dateOfBirth?: string;
+  avatar?: string;
   address?: string;
   skills: { id: number; name: string }[];
   role: 'STUDENT' | 'COMPANY' | 'ADMIN';
 }
 export interface UpdateProfilePayload {
-  id?: number;       
-  name: string;     
-  dateOfBirth: string;  
+  id?: number;
+  name: string;
+  phone?: string;
+  dateOfBirth?: string;
+  dob?: string;
   address: string;
   gender: 'MALE' | 'FEMALE' | 'OTHER';
-
+  skills?: number[];
 }

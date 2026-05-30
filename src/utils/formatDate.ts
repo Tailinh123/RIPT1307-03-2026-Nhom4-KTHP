63583821c6ce499294f830197907a820c60487b1
lutrongdai <lutrongd@gmail.com>
2026-05-30 07:18:47 +0700

    update import

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,6 +1,8 @@
 import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
 import 'dayjs/locale/vi';
 
+dayjs.extend(relativeTime);
 dayjs.locale('vi');
 
 export function formatDate(date: string | Date, template = 'DD/MM/YYYY'): string {
